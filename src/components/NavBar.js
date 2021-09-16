6ca021b6e6f50a3871643c0ba4d4405cdfebd321
Daniel Calvin <arockne2003@yahoo.com>
2021-09-16 10:11:47 -0700

    Move NavBar component from App and export NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav>
+      <button>Home</button>
+      <button>Inventory</button>
+      <button>New</button>
+    </nav>
+  )
+}
+
+export default NavBar
