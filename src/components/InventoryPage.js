588a3286af5b39857d438929c19275c281b34918
Daniel Calvin <arockne2003@yahoo.com>
2021-09-16 11:57:49 -0700

    Fetch inventory data and store in state

--- src/components/InventoryPage.js
+++ src/components/InventoryPage.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './Search'
 import Item from './Item'
 
 function InventoryPage() {
+  const [masterInventory, setMasterInventory] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3004/inventory')
+    .then(r => r.json())
+    .then(setMasterInventory)
+  }, [])
+  
   return (
     <div className='inventory-page'>
       <Search />
+      
     </div>
   )
 }
