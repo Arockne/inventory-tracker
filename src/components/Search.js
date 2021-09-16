f4cef72fb579179706aa06493fcc9a5a1ea5dd0a
Daniel Calvin <arockne2003@yahoo.com>
2021-09-16 10:12:45 -0700

    Move Search component from App and export Search

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <form>
+     <input type="text" placeholder="Search By Name.."/>
+     <input type="submit" />
+    </form>
+  )
+}
+
+export default Search
