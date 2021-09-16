999045b0bbbe9495ab1227e6a0e3594f1a69a465
Daniel Calvin <arockne2003@yahoo.com>
2021-09-16 10:12:15 -0700

    Move NewItem component from App and export NewItem

---
+++ src/components/NewItem.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function NewItem() {
+  return (
+    <form>
+      <label>Name:
+        <input type="text" name="name" id="name" placeholder="Name..."/>
+      </label>
+      <label>Image:
+        <input type="text" name="image" id="image" placeholder="Image..."/>
+      </label>
+      <label>Category:
+        <select name="category">
+          <option value="">-- Choose a Category --</option>
+          <option value="meat">Meat</option>
+          <option value="poultry">Poultry</option>
+          <option value="seafood">Seafood</option>
+          <option value="dairy">Dairy</option>
+          <option value="produce">Produce</option>
+          <option value="bakery">Bakery and Desserts</option>
+          <option value="supplies">Supplies</option>
+          <option value="beverages">Beverages</option>
+          <option value="pantry">Pantry Staples</option>
+        </select>
+      </label>
+      <input type="submit" value="Add Item"/>
+    </form>
+  )
+}
+
+export default NewItem
