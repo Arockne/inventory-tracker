2b1c6083ee3fe89efc410b6ef5c28e5ab8c8f7bc
Daniel Calvin <arockne2003@yahoo.com>
2021-09-16 08:50:45 -0700

    Implement Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
+function Header() {
+  return (
+    <div>
+      <h1>Inventory Tracker<span>🍌🍇🥬</span></h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      
+      {
+        /*
+        Header
+        Navbar
+        InventoryPage
+        New
+        */
+       <Header />
+      }
     </div>
   );
 }
