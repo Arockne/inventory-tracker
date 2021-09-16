a0f6cb85ef97208a6cd27042641061014ec9bc90
Daniel Calvin <arockne2003@yahoo.com>
2021-09-16 09:25:34 -0700

    Implement NewItem component

--- src/components/App.js
+++ src/components/App.js
@@ -33,6 +33,34 @@ function Home() {
   )
 }
 
+function NewItem() {
+  return (
+    <form>
+      <label>Name:
+        <input type="text" name="name" id="name" placeholder="Name..."/>
+      </label>
+      <label>Image:
+        <input type="text" name="image" id="image" placeholder="Image..."/>
+      </label>
+      <label>Category:
+        <select name="category">
+          <option value="">-- Choose a Category --</option>
+          <option value="meat">Meat</option>
+          <option value="poultry">Poultry</option>
+          <option value="seafood">Seafood</option>
+          <option value="dairy">Dairy</option>
+          <option value="produce">Produce</option>
+          <option value="bakery">Bakery and Desserts</option>
+          <option value="supplies">Supplies</option>
+          <option value="beverages">Beverages</option>
+          <option value="pantry">Pantry Staples</option>
+        </select>
+      </label>
+      <input type="submit" value="Add Item"/>
+    </form>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -47,6 +75,7 @@ function App() {
       <Header />
       <Navbar />
       <Home />
+      <NewItem />
     </div>
   );
 }
