9fea8be66b7e69060e8e1ff0de597773b627dfb1
Daniel Calvin <arockne2003@yahoo.com>
2021-09-18 14:32:31 -0700

    Add more info to card

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import { Card, Form, Image, Input } from 'semantic-ui-react'
 
+
+/*
+    {
+      "id": 3, 
+      "name": "peas",
+      "category": "produce", 
+      "unitMeasurement": "lb", 
+      "amount": 1,
+      "image": "https://images.unsplash.com/photo-1615485500710-aa71300612aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+    }
+*/
 function Item({ item }) {
-  const { id, name, category, image } = item
+  const { id, name, category, amount, unitMeasurement, image } = item
   const itemName = `${name[0].toUpperCase()}${name.slice(1)}`
   return (
     <Card>
       <Image src={image} size="large" />
       <Card.Content>
         <Card.Header>{itemName}</Card.Header>
-        <Card.Description>{`Category: ${category}`}</Card.Description>
-        <Form size="tiny">
-          <Input type="number" min="1" />
-          <Input type="submit" value="Add"/>
-        </Form>
+        <Card.Description>
+          {
+            `
+              Category: ${category}
+              Amount: ${amount}${unitMeasurement}
+            `
+          }
+        </Card.Description>
       </Card.Content>
     </Card>
   )
