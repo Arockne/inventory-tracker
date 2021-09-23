175392d12f8fbb71cb6d345f9bbcb4d83a008dce
Daniel Calvin <arockne2003@yahoo.com>
2021-09-22 19:15:25 -0700

    Implement TableTotal to use passed in array's elements

--- src/components/TableTotal.js
+++ src/components/TableTotal.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Header, Table } from 'semantic-ui-react'
 
-function TableTotal() {
+function TableTotal({ categoryTotal }) {
+  const [category, total] = categoryTotal
+  const label = category.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
   return (
     <Table.Row>
       <Table.Cell>
         <Header as="h4">
-          <Header.Content></Header.Content>
+          <Header.Content>{label}</Header.Content>
         </Header>
       </Table.Cell>
-      <Table.Cell></Table.Cell>
+      <Table.Cell>{`$${total}`}</Table.Cell>
     </Table.Row>
   )
 }
