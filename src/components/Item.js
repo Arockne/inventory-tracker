ac3b53de5b85101cf1fc7d6a25b6bf066500ef9a
Daniel Calvin <arockne2003@yahoo.com>
2021-09-24 10:57:48 -0700

    Refactored using totalCost and capitalize helper functions

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Card, Image, Icon } from 'semantic-ui-react'
 import { Link } from "react-router-dom";
+import { totalCost, capitalize } from '../helpers'
 
 function Item({ item }) {
-  const { name, category, amount, unitMeasurement, image, pricePerUnit } = item
-  const itemName = name.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
-  const totalCost = (Math.round(((parseFloat(pricePerUnit) * parseFloat(amount)) + Number.EPSILON) * 100)) / 100
+  const { name, category, amount, unitMeasurement, image } = item
 
   function cardColor({ category }) {
     switch(category) {
@@ -37,14 +36,14 @@ function Item({ item }) {
       <Image src={image} size="massive"/>
       <Card.Content>
         <Card.Header>
-          {itemName}
+          {capitalize(name)}
           <Link to={`./edit/${name}`}>
             <Icon name="write" size="small" color="black"/>
           </Link>
         </Card.Header>
         <Card.Description content={`Category: ${category}`} />
         <Card.Description content={`Amount: ${amount}${unitMeasurement}`} />
-        <Card.Description content={`Total Cost: $${totalCost}`} />
+        <Card.Description content={`Total Cost: $${totalCost(item)}`} />
       </Card.Content>
     </Card>
   )
