785cbe86284eb0fe48eb74789fdb370c5f3e91a0
Daniel Calvin <arockne2003@yahoo.com>
2021-09-24 10:46:45 -0700

    Implement capitalize and totalCost

---
+++ src/helpers.js
@@ -0,0 +1,5 @@
+const capitalize = (name) => name.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+
+const totalCost = ({ pricePerUnit, amount }) => (Math.round(((parseFloat(pricePerUnit) * parseFloat(amount)) + Number.EPSILON) * 100)) / 100
+
+export { capitalize, totalCost }
