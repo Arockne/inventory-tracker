d3043f66591c10e75d902a9a61e2c66dc646a7b1
Daniel Calvin <arockne2003@yahoo.com>
2021-09-24 11:13:06 -0700

    Rename level 2 heading to Current Inventory Expenses

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import TableTotals from './TableTotals'
 function Home({ inventory }) {
   return (
     <div className="home">
-      <h2>Welcome to my inventory food tracker</h2>
+      <h2>Current Inventory Expenses</h2>
       <TableTotals inventory={inventory}/>
     </div>
   )
