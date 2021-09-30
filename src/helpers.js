8fea5ed3a8ede713737ff2895bf9d5e0bf4ff14c
Daniel Calvin <arockne2003@yahoo.com>
2021-09-30 09:42:39 -0700

    Fix bug on calculating totalCost

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
 const capitalize = (name) => name.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
 
-const totalCost = ({ pricePerUnit, amount }) => (Math.round(((parseFloat(pricePerUnit) * parseFloat(amount)) + Number.EPSILON) * 100)) / 100
+const totalCost = ({ pricePerUnit, amount }) => (Math.round(((parseFloat(pricePerUnit) * parseFloat(amount))) * 100)) / 100
 
 export { capitalize, totalCost }
